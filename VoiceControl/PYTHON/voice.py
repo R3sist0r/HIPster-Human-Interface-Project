272f31d84e6c1cf84d0cf65714cc10a990441740
Reuben Posthuma <reuben.posthuma@gmail.com>
2011-03-30 21:42:07 +1300

    Made a few changes, mostly to obey DRY

--- VoiceControl/PYTHON/voice.py
+++ VoiceControl/PYTHON/voice.py
@@ -1,15 +1,33 @@
 import speech
+import serial
 
-def commandLED(phraseID): 
-	print phraseID
+ser = serial.Serial('COM3', 9600, timeout=1)
 
-phrases = ['Max Red', 'Increase Ruby', 'Decrease Ruby', 'Increase Green', 'Decrease Green', 'Increase Blue', 'Decrease Blue', 'Off', 'L E D white', 'turn off']
+def commandLED(phraseID):
+    print phraseID
+    result = {
+            0: 'R',     #! Red max
+            1: 'r',     #! Red up
+            2: 'e',     #! Red down
+            3: 'E',     #! Red off
+            4: 'G',     #! Green max
+            5: 'g',     #! Green up
+            6: 'f',     #! Green down
+            7: 'F',     #! Green off
+            8: 'B',     #! Blue max
+            9: 'b',     #! Blue up
+            10: 'v',    #! Blue down
+            11: 'V',    #! Blue off
+            12: 'o',    #! All off
+            13: 'w'     #! All on (white)
+    }[phraseID]
+    print result
+    
+phrases = ['Max Ruby', 'Increase Ruby', 'Decrease Ruby', 'Minimum Ruby', 'Max Green', 'Increase Green', 'Decrease Green', 'Minimum Green', 'Max Blue', 'Increase Blue', 'Decrease Blue', 'Min Blue', 'Off', 'L E D white']
 while True:
-	phrase = speech.input("Control the LED", phrases )
-	print phrase	
-	commandLED(phrases.index(phrase))
-	if phrase == "turn off":
-		break
-		
+    phrase = speech.input("Control the LED", phrases )
+    print phrase
+    commandLED(phrases.index(phrase)) 
+#ser.close()
 
-	
+
