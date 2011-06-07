5f712717e06c0595537390ba83d2a1f3c18d4b14
Reuben Posthuma <reuben.posthuma@gmail.com>
2011-06-07 16:43:44 +1200

    Got the adc rgb-led control working. The system cycles through the first 4 adc pins (ADC0-ADC3), each of which has a potentiometer connected to it.  Depending on the value of each potentiometer, it changes the related value of the led colour.

--- ADC/adc.c
+++ ADC/adc.c
@@ -1,14 +1,42 @@
 #include "define.h"
+
 #define ADC_BRIGHTNESS    0
 #define ADC_RED           _BV(MUX0)
 #define ADC_GREEN         _BV(MUX1)
 #define ADC_BLUE          _BV(MUX1)|_BV(MUX0)
+#define ADMUX_CLR() {ADMUX &= !_BV(MUX1)&!_BV(MUX0);}
+enum {BRIGHTNESS, RED, GREEN, BLUE}adc_next;
+enum {new, old}adc_state;
 
 void adc_init(void) {
   ADCSRA |= _BV(ADEN)|_BV(ADATE)|_BV(ADIE);   //Enable ADC with free running mode, and enable the interrupt service routine
   ADMUX |= _BV(REFS0)|_BV(ADLAR);             //Select AVCC as a voltage reference, and enable left adjust to give 8bit resolution
+  adc_next = BRIGHTNESS;
+  adc_state = new;
 }
 
-ISR(ADC_VECT) {
-  
+void adc_startConversion(void) {
+  ADCSRA |= _BV(ADSC);    //start conversion
+}
+
+ISR(ADC_vect) {
+  char mode = (ADMUX&0x0F);
+  switch(mode) {
+    case BRIGHTNESS:
+        ADMUX++;
+		pwm_setBrightness(ADCH);
+    break;
+    case RED:
+      ADMUX++;
+      pwm_sendRed(ADCH);
+    break;
+    case GREEN:
+      ADMUX++;
+      pwm_sendGreen(ADCH);
+    break;
+    case BLUE:
+      ADMUX &= 0xF8;
+      pwm_sendBlue(ADCH);
+    break;
+  }
 }
